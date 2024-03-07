cadbebf28ab81af863cc31613cf6ef6949876098
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 12:35:35 +0300

    Add Customer Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Page from "./component/page/Page.jsx";
+import {customerTemplate} from "./template/EntityTemplates.js";
 
 function App() {
   return (
-    <>
-
-    </>
+    <div className="app">
+        <Routes>
+            <Route path="/customers" element={<Page entityTemplate={customerTemplate}/>}/>
+        </Routes>
+    </div>
   )
 }
 
