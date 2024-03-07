f93d2455e217022e54f5ea0ef07eb9c5f54d0dca
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 12:36:33 +0300

    Map Cards

--- src/component/page/Page.jsx
+++ src/component/page/Page.jsx
@@ -1,9 +1,31 @@
 import "./Page.style.css"
+import {useEffect, useState} from "react";
+import Card from "../card/Card.jsx";
 
 const Page = ({entityTemplate}) => {
+    const [entities, setEntities] = useState([])
+
+    useEffect(() => {
+        entityTemplate.getFunction().then(data => {
+            setEntities(data)
+        })
+    }, []);
+
     return (
         <div className="page">
             <h2>{entityTemplate.name} Management</h2>
+
+            <div className="page-content-container">
+                <h3>{entityTemplate.name} List</h3>
+
+                {entities.map(entity => {
+                    return (
+                        <div key={entity.id} className="card-container">
+                            <Card entity={entity} inputs={entityTemplate.inputs}/>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 
