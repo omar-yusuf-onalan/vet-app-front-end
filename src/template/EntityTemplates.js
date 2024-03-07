6e4b18b3021c0fae8ce9ca7b91d36475f32def05
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 22:02:41 +0300

    Refactor file

--- src/template/EntityTemplates.js
+++ src/template/EntityTemplates.js
@@ -1,51 +1,7 @@
-import {deleteCustomer, getCustomers, updateCustomer} from "../api/CustomerService.js";
+import customerTemplate from "./CustomerTemplate.js";
+import animalTemplate from "./AnimalTemplate.js";
 
 export const entityTemplates = [
-    {
-    name: "Customer",
-    path: "/customers",
-    inputs: [
-        {
-            placeholder: "Name",
-            type: "text",
-            value: "name",
-            name: "name",
-            title: "Name"
-        },
-        {
-            placeholder: "Email",
-            type: "text",
-            value: "email",
-            name: "email",
-            title: "Email"
-        },
-        {
-            placeholder:"Address",
-            type: "text",
-            value: "address",
-            name: "address",
-            title: "Address"
-        },
-        {
-            placeholder: "City",
-            type: "text",
-            value: "city",
-            name: "city",
-            title: "City"
-        },
-        {
-            placeholder: "Phone",
-            type: "text",
-            value: "phone",
-            name: "phone",
-            title: "Phone"
-        }],
-    parents: [],
-    children: [],
-    changeHandlerFunction: {},
-    getFunction: getCustomers,
-    createFunction: {},
-    updateFunction: updateCustomer,
-    deleteFunction: deleteCustomer
-}
+    customerTemplate,
+    animalTemplate
 ]
