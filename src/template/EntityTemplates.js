e000de12fe9638ea5e0b49946bdd4f15a93074de
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 11:12:43 +0300

    Create EntityTemplates.js

---
+++ src/template/EntityTemplates.js
@@ -0,0 +1,11 @@
+export const customerTemplate = {
+    name: "Customer",
+    inputs: ["Name", "Email", "Address", "City", "Phone"],
+    parents: [],
+    changeHandlerFunction: {},
+    getHandlerFunction: {},
+    createHandlerFunction: {},
+    updateHandlerFunction: {},
+    deleteHandlerFunction: {}
+
+}
