8cd4dd1e63f959d6fd444274ee93fa2c4e983edd
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 19:37:44 +0300

    Create AddContent.jsx

---
+++ src/component/addcontent/AddContent.jsx
@@ -0,0 +1,11 @@
+import "./AddContent.style.css"
+
+const AddContent = ({entityTemplate}) => {
+    return (
+        <div className="add-content">
+
+        </div>
+    )
+}
+
+export default AddContent
