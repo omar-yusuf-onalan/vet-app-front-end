fab23dc5addaa1390d73703f14e51a6ecd2ae9e1
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 13:49:06 +0300

    Implement user input and updating and deleting entities

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,22 +1,44 @@
 import "./Card.style.css"
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import Updater from "../updater/Updater.jsx";
+import {handleDelete, handleUpdate} from "../../handler/CRUDHandler.js";
+import Deleter from "../deleter/Deleter.jsx";
 
-const Card = ({entity, inputs}) => {
+const Card = ({entity, inputs, setEntities, entityTemplate}) => {
+    const [entityInCard, setEntityInCard] = useState(entity)
+
+    const handleChange = (event) => {
+        setEntityInCard({...entityInCard, [event.target.name]: event.target.value})
+    };
+
     return (
         <div className="card">
             {inputs.map(input => {
                 return (
-                    <div key={input.placeholder} className="card-content">
+                    <div key={input.placeholder}>
                         <h3>{input.title}</h3>
                         <input
+                            onChange={handleChange}
                             placeholder={input.placeholder}
                             type={input.type}
                             title={input.title}
+                            name = {input.name}
+                            value={entityInCard[input.value]}
                         />
+
                     </div>
+
                 )
                 }
             )}
+
+            <Updater handlerFunction={
+                () => handleUpdate(entityInCard, setEntities, entityTemplate.updateFunction)}
+            />
+
+            <Deleter handlerFunction={
+                () => handleDelete(entityInCard, setEntities, entityTemplate.deleteFunction)}
+            />
         </div>
     )
 }
