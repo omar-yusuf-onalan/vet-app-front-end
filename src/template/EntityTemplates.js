b68ac96b909dbf915d420e19467fdd37debb9563
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 12:35:47 +0300

    Add to CustomerTemplate

--- src/template/EntityTemplates.js
+++ src/template/EntityTemplates.js
@@ -1,9 +1,42 @@
+import {getCustomers} from "../api/CustomerService.js";
+
 export const customerTemplate = {
     name: "Customer",
-    inputs: ["Name", "Email", "Address", "City", "Phone"],
+    inputs: [
+        {
+            placeholder: "Name",
+            type: "text",
+            value: "name",
+            title: "Name"
+        },
+        {
+            placeholder: "Email",
+            type: "text",
+            value: "email",
+            title: "Email"
+        },
+        {
+            placeholder:"Address",
+            type: "text",
+            value: "address",
+            title: "Address"
+        },
+        {
+            placeholder: "City",
+            type: "text",
+            value: "city",
+            title: "City"
+        },
+        {
+            placeholder: "Phone",
+            type: "text",
+            value: "phone",
+            title: "Phone"
+        }],
     parents: [],
+    children: [],
     changeHandlerFunction: {},
-    getHandlerFunction: {},
+    getFunction: getCustomers,
     createHandlerFunction: {},
     updateHandlerFunction: {},
     deleteHandlerFunction: {}
