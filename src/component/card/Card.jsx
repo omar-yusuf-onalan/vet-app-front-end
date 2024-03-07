fcdc9b783051caed1a32b2e6deec1331d7205964
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 12:36:38 +0300

    Map inputs

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,9 +1,22 @@
 import "./Card.style.css"
+import {useEffect} from "react";
 
-const Card = () => {
+const Card = ({entity, inputs}) => {
     return (
         <div className="card">
-
+            {inputs.map(input => {
+                return (
+                    <div key={input.placeholder} className="card-content">
+                        <h3>{input.title}</h3>
+                        <input
+                            placeholder={input.placeholder}
+                            type={input.type}
+                            title={input.title}
+                        />
+                    </div>
+                )
+                }
+            )}
         </div>
     )
 }
