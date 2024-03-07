af23be1f8c57fd96e6bf82bc7179d5d10e12eaef
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-07 12:35:57 +0300

    Create CustomerService.js

---
+++ src/api/CustomerService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export const getCustomers = async () => {
+    const { data } = await axios.get(
+        import.meta.env.VITE_APP_BASE_URL + `customers`
+    );
+
+    return data;
+};
+
+export const deleteCustomer = async (id) => {
+    const { data } = await axios.delete(
+        import.meta.env.VITE_APP_BASE_URL + `customers/` + id
+    );
+
+    return data;
+};
+
+export const createCustomer = async (customer) => {
+    const { data } = await axios.post(
+        import.meta.env.VITE_APP_BASE_URL + `customers`,
+        customer
+    );
+
+    return data;
+};
+
+export const updateCustomer = async (customer) => {
+    const { data } = await axios.put(
+        import.meta.env.VITE_APP_BASE_URL + `customers`,
+        customer
+    );
+
+    return data;
+};
