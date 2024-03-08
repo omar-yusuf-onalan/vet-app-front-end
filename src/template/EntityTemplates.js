edfeff21a480182f3fc5c3e5c981666daa84fd94
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-08 21:31:07 +0300

    Add templates

--- src/template/EntityTemplates.js
+++ src/template/EntityTemplates.js
@@ -1,7 +1,15 @@
 import customerTemplate from "./CustomerTemplate.js";
 import animalTemplate from "./AnimalTemplate.js";
+import doctorTemplate from "./DoctorTemplate.js";
+import appointmentTemplate from "./AppointmentTemplate.js";
+import reportTemplate from "./ReportTemplate.js";
+import vaccineTemplate from "./VaccineTemplate.js";
 
 export const entityTemplates = [
     customerTemplate,
-    animalTemplate
+    animalTemplate,
+    doctorTemplate,
+    appointmentTemplate,
+    reportTemplate,
+    vaccineTemplate
 ]
