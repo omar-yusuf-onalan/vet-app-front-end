3a35b23d171490b6294380202eb1f72be0e4dfac
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-08 21:29:44 +0300

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,35 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <header>
+                <h1>Welcome to Veterinary Management System!</h1>
+            </header>
+
+            <section>
+                <h2>About The Project:</h2>
+                <p>Our Veterinary Management System aims to streamline the operations of veterinary clinics by providing
+                    comprehensive backend and frontend solutions. Whether you're managing appointments, tracking
+                    vaccinations, or maintaining customer and animal records, our system is designed to make your
+                    workflow more efficient.</p>
+            </section>
+
+            <section>
+                <h2>What It Offers:</h2>
+                <ul>
+                    <li><strong>Efficient Backend Operations:</strong> We offer a robust backend system that allows for
+                        seamless management of veterinarian doctors, customer information, animal records, appointments,
+                        vaccinations, and more.
+                    </li>
+                    <li><strong>User-Friendly Frontend Interface:</strong> Our frontend interface is designed with user
+                        experience in mind, providing intuitive navigation and easy access to all functionalities.
+                    </li>
+                    <li><strong>CRUD Operations:</strong> With Create, Read, Update, and Delete functionalities
+                        available for all entities, you have full control over your data.
+                    </li>
+                </ul>
+            </section>
+        </div>
+    )
+}
+
+export default Home
