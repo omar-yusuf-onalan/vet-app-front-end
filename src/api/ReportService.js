9bb2898bb96df5d8a99c746408fd8661b8f191cf
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-08 21:30:20 +0300

    Create ReportService.js

---
+++ src/api/ReportService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export const getReports = async () => {
+    const { data } = await axios.get(
+        import.meta.env.VITE_APP_BASE_URL + `reports`
+    );
+
+    return data;
+};
+
+export const deleteReport = async (id) => {
+    const { data } = await axios.delete(
+        import.meta.env.VITE_APP_BASE_URL + `reports/` + id
+    );
+
+    return data;
+};
+
+export const createReport = async (report) => {
+    const { data } = await axios.post(
+        import.meta.env.VITE_APP_BASE_URL + `reports`,
+        report
+    );
+
+    return data;
+};
+
+export const updateReport = async (report) => {
+    const { data } = await axios.put(
+        import.meta.env.VITE_APP_BASE_URL + `reports`,
+        report
+    );
+
+    return data;
+};
