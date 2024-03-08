0d9727c264e2edb87b02131b7c0606f3bc9d829c
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-08 21:30:12 +0300

    Create AvailableDateService.js

---
+++ src/api/AvailableDateService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export const getAvailableDates = async () => {
+    const { data } = await axios.get(
+        import.meta.env.VITE_APP_BASE_URL + `availableDates`
+    );
+
+    return data;
+};
+
+export const deleteAvailableDate = async (id) => {
+    const { data } = await axios.delete(
+        import.meta.env.VITE_APP_BASE_URL + `availableDates/` + id
+    );
+
+    return data;
+};
+
+export const createAvailableDate = async (availableDate) => {
+    const { data } = await axios.post(
+        import.meta.env.VITE_APP_BASE_URL + `availableDates`,
+        availableDate
+    );
+
+    return data;
+};
+
+export const updateAvailableDate = async (availableDate) => {
+    const { data } = await axios.put(
+        import.meta.env.VITE_APP_BASE_URL + `availableDates`,
+        availableDate
+    );
+
+    return data;
+};
