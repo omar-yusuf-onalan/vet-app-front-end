215f1f2c30ebaa49840f03944764c55ba8e9a58e
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-03-10 21:01:10 +0300

    Add showError and errorMessage

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import {Route, Routes} from "react-router-dom";
 import Page from "./component/page/Page.jsx";
 import {entityTemplates} from "./template/EntityTemplates.js";
 import Navbar from "./component/navbar/Navbar.jsx";
-import {useState} from "react";
+import {useContext, useState} from "react";
 import Home from "./Home.jsx";
+import ErrorContext from "./context/error/ErrorContext.jsx";
 
 function App() {
     const [refreshContent, setRefreshContent] = useState(false)
 
+    const {showError, errorMessage} = useContext(ErrorContext)
+
     return (
     <div className="app">
         <Navbar setRefreshContent={setRefreshContent} />
+
+        {showError && <div>{errorMessage}</div>}
 
         <Routes>
             <Route path="/" element={<Home />}></Route>
